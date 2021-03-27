9585689deb5c6fda5264eda74eb7b97c2d39fc4b
Massimiliano Marcon <me@marcon.me>
2021-03-27 15:30:55 +0100

    Supported a few API methods down to webhooks

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 const logger = require('pino')({ level: 'debug' });
 const process = require('process');
-const { login, profile, groupIds } = require('./lib/auth')(fetch, { logger: logger.child({ module: 'auth' }) });
-// const { groups } = require('./lib/groups')(fetch, { logger: logger.child({ module: 'groups' }) });
+const { login, groupIds } = require('./lib/auth')(fetch, { logger: logger.child({ module: 'auth' }) });
+const { getApps } = require('./lib/groups/apps')(fetch, { logger: logger.child({ module: 'groups' }) });
+const { getServices } = require('./lib/groups/apps/services')(fetch, { logger: logger.child({ module: 'services' }) });
+const { getIncomingWebhooks } = require('./lib/groups/apps/services/incoming_webooks')(fetch, { logger: logger.child({ module: 'incoming_webhooks' }) });
 
 (async function () {
   const user = await login(process.env);
-  const userProfile = await groupIds(user);
-  // const groupsInfo = await groups(user);
-  console.log(userProfile);
+  const groups = await groupIds(user);
+  const apps = await getApps({ ...user, groupId: groups[0] });
+  const services = await getServices({ ...user, groupId: groups[0], appId: apps.find(({ name }) => name === 'iot-fun')?.appId });
+  const incomingWebhooks = await getIncomingWebhooks({
+    ...user,
+    groupId: groups[0],
+    appId: apps.find(({ name }) => name === 'iot-fun')?.appId,
+    serviceId: services.find(({ name }) => name === 'alexa')?.serviceId
+  });
+  logger.info(incomingWebhooks);
 })();
